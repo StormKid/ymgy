be49662cbc493f3bb18013415216683ddd9444d0
ke_li <stokid@126.com>
2019-08-12 17:29:31 +0800

    change some

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Transport } from '@nestjs/common/enums/transport.enum';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.connectMicroservice({
+     transport: Transport.TCP,
+     options : {
+         retryAttempts : 100,
+         retryDelay : 10000,
+     },
+  });
+
+  await app.startAllMicroservicesAsync();
+  await app.listen(7897);
 }
 bootstrap();
